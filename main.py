67a2fdd9727ddbc097da0908b3b499499a86c294
Olof <olof.konton@tutanota.com>
2022-02-15 17:23:07 +0100

    Added help function and fixed aliases for kom.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from discord import Embed, Color
 from discord.ext import commands
 
 def read_token():
@@ -14,39 +15,47 @@ bot = commands.Bot(command_prefix=read_prefix())
 async def ping(ctx):
     await ctx.message.channel.send("Pong!")
 
-@bot.command(name = "prefix", aliases = ["pix", "fax"])
+@bot.command(name = "prefix")
 async def prefix(ctx, new_prefix):
     bot.command_prefix = new_prefix
     with open('prefix.txt', "w") as f:
         f.write(new_prefix)
     await ctx.message.channel.send("Tack för mitt nya prefix! 🥰")
 
-@bot.command(name = "kom", aliases = ["komsi komsi", "älskling jag är hemma", "hit"])
-async def kom(ctx):
-    # If user is in a channel
-    if ctx.author.voice:
-        # If bot is in a channel
-        if ctx.guild.voice_client:
-            # If they are in the same channel
-            if ctx.author.voice.channel == ctx.guild.voice_client.channel:
-                await ctx.send("Jag är redan här för dig! 🥰")
-            # If they are in different channels
-            else:
-                # If bot has company
-                if len(ctx.guild.voice_client.channel.members) > 1:
-                    await ctx.send("Jag är upptagen! 😤")
-                # If bot is alone
+@bot.command(name = "kom", aliases = ["komsi", "älskling", "hit"])
+async def kom(ctx, arg1="", arg2="", arg3=""):
+    cm = ctx.invoked_with
+    await ctx.send("cm: " + cm + "\narg1: " + arg1 + "\narg2: " + arg2 + "\narg3: " + arg3)
+    if (cm == "kom"
+        or (cm == "hit")
+        or (cm == "komsi" and arg1 == "komsi")
+        or (cm == "älskling" and arg1 == "jag" and arg2 == "är" and arg3 == "hemma")):
+        # If user is in a channel
+        if ctx.author.voice:
+            # If bot is in a channel
+            if ctx.guild.voice_client:
+                # If they are in the same channel
+                if ctx.author.voice.channel == ctx.guild.voice_client.channel:
+                    await ctx.send("Jag är redan här för dig! 🥰")
+                # If they are in different channels
                 else:
-                    await ctx.send("Äntligen lite sällskap igen! 😊")
-                    await ctx.guild.voice_client.disconnect()
-                    await ctx.author.voice.channel.connect()
-        # If user is in a channel, but bot is not
+                    # If bot has company
+                    if len(ctx.guild.voice_client.channel.members) > 1:
+                        await ctx.send("Jag är upptagen! 😤")
+                    # If bot is alone
+                    else:
+                        await ctx.send("Äntligen lite sällskap igen! 😊")
+                        await ctx.guild.voice_client.disconnect()
+                        await ctx.author.voice.channel.connect()
+            # If user is in a channel, but bot is not
+            else:
+                await ctx.send("Jag kommer! 😁")
+                await ctx.author.voice.channel.connect()
+        # If user is not in a channel
         else:
-            await ctx.send("Jag kommer! 😁")
-            await ctx.author.voice.channel.connect()
-    # If user is not in a channel
+            await ctx.send("Jag vet inte vart jag ska. 😢")
     else:
-        await ctx.send("Jag vet inte vart jag ska. 😢")
+        pass
 
 @bot.command(name = "stick", aliases = ["schas", "försvinn", "dra", "gå"])
 async def stick(ctx):
@@ -80,5 +89,12 @@ async def stick(ctx):
     else:
         await ctx.send("Jag har redan stuckit ju! 💔")
 
+@bot.command(name = "hjälp", aliases = ["hilfe", "aidez-moi", "h"])
+async def hjälp(ctx):
+    embed=Embed(title="Mina kommandon 😎 :", color=Color.orange(), description = f"Genom att skriva \"{bot.command_prefix}\" följt av ett av nedanstående kommandon kan du få mig att göra roliga saker! Lek med mig! 🥰")
+    embed.add_field(name="**stick / gå / schas / försvinn / dra**", value="Säg åt mig att lämna röstkanalen. 😥", inline=False)
+    embed.add_field(name="**kom / hit / komsi komsi / älskling jag är hemma**", value="Be mig att göra dig sällskap! 😇", inline=False)
+    embed.add_field(name="**prefix**", value="Ge mig ett nytt prefix som jag kan lyssna på! ☺️")
+    await ctx.send(embed=embed)
 
 bot.run(read_token())
