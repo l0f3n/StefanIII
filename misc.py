6c90cb134eec16964eed6759e35dae6fec0007c4
Victor Löfgren <viclo211@student.liu.se>
2022-03-13 13:46:40 +0100

    Put rest of commands in misc cog

---
+++ misc.py
@@ -0,0 +1,103 @@
+from discord import Embed
+from discord.ext import commands
+
+from config import config
+
+class Misc(commands.Cog):
+    
+    def __init__(self, bot) -> None:
+        super().__init__()
+
+        self.bot = bot
+
+    async def close(self):
+        pass
+
+    @commands.command()
+    async def ping(self, ctx):
+        """ TODO: Write docstring """
+        await ctx.message.channel.send("Pong!")
+
+    @commands.command(name = "prefix")
+    async def prefix(self, ctx, new_prefix):
+        """ TODO: Write docstring """
+
+        self.command_prefix = new_prefix
+        await config.set("prefix", new_prefix)
+
+        await ctx.message.channel.send("Tack för mitt nya prefix! 🥰")
+
+    @commands.command(name = "kom", aliases = ["komsi", "älskling", "hit"])
+    async def kom(self, ctx, arg1="", arg2="", arg3=""):
+        """ TODO: Write docstring """
+        cm = " ".join(filter(None, [ctx.invoked_with, arg1, arg2, arg3]))
+        # await ctx.send("cm: " + cm + "\narg1: " + arg1 + "\narg2: " + arg2 + "\narg3: " + arg3)
+        if cm in ["kom", "kom hit", "komsi komsi", "älskling jag är hemma", "hit"]:
+            # If user is in a channel
+            if ctx.author.voice:
+                # If bot is in a channel
+                if ctx.guild.voice_client:
+                    # If they are in the same channel
+                    if ctx.author.voice.channel == ctx.guild.voice_client.channel:
+                        await ctx.send("Jag är redan här för dig! 🥰")
+                    # If they are in different channels
+                    else:
+                        # If bot has company
+                        if len(ctx.guild.voice_client.channel.members) > 1:
+                            await ctx.send("Jag är upptagen! 😤")
+                        # If bot is alone
+                        else:
+                            await ctx.send("Äntligen lite sällskap igen! 😊")
+                            await ctx.guild.voice_client.disconnect()
+                            await ctx.author.voice.channel.connect()
+                # If user is in a channel, but bot is not
+                else:
+                    await ctx.send("Jag kommer! 😁")
+                    await ctx.author.voice.channel.connect()
+            # If user is not in a channel
+            else:
+                await ctx.send("Jag vet inte vart jag ska. 😢")
+        else:
+            pass
+
+    @commands.command(name = "stick", aliases = ["schas", "försvinn", "dra", "gå"])
+    async def stick(self, ctx):
+        """ TODO: Write docstring """
+        # If bot is in a channel
+        if ctx.guild.voice_client:
+            # If user is in a channel
+            if ctx.author.voice:
+                # If they are in the same channel
+                if ctx.author.voice.channel == ctx.guild.voice_client.channel:
+                    await ctx.send('Okej då... 😥')
+                    await ctx.guild.voice_client.disconnect()
+                # If they are in different channels
+                else:
+                    # If bot has company
+                    if len(ctx.guild.voice_client.channel.members) > 1:
+                        await ctx.send("Förstör inte det roliga! 😠")
+                    # If bot is alone
+                    else:
+                        await ctx.send('Okej då... 😥')
+                        await ctx.guild.voice_client.disconnect()
+            # If bot is in a channel, but user is not
+            else:
+                # If bot has company
+                if len(ctx.guild.voice_client.channel.members) > 1:
+                    await ctx.send("Förstör inte det roliga! 😠")
+                # If bot is alone
+                else:
+                    await ctx.send('Okej då... 😥')
+                    await ctx.guild.voice_client.disconnect()
+        # If bot is not in a channel
+        else:
+            await ctx.send("Jag har redan stuckit ju! 💔")
+
+    @commands.command(name = "hjälp", aliases = ["hilfe", "aidez-moi", "h"])
+    async def hjälp(self, ctx):
+        """ TODO: Write docstring """
+        embed=Embed(title="Mina kommandon 😎 :", color=Color.orange(), description = f"Genom att skriva \"{self.command_prefix}\" följt av ett av nedanstående kommandon kan du få mig att göra roliga saker! Lek med mig! 🥰")
+        embed.add_field(name="**stick / gå / schas / försvinn / dra**", value="Säg åt mig att lämna röstkanalen. 😥", inline=False)
+        embed.add_field(name="**kom / hit / komsi komsi / älskling jag är hemma**", value="Be mig att göra dig sällskap! 😇", inline=False)
+        embed.add_field(name="**prefix**", value="Ge mig ett nytt prefix som jag kan lyssna på! ☺️")
+        await ctx.send(embed=embed)
--- stefan.py
+++ stefan.py
@@ -1,9 +1,9 @@
 import discord
-from discord import Embed, Color
 from discord.ext import commands
 
-from music import Music
 from config import config
+from music import Music
+from misc import Misc
 
 class Stefan(commands.Bot):
     
@@ -15,7 +15,6 @@ class Stefan(commands.Bot):
         
         self.before_invoke(self._handle_before_invoke)
         self.after_invoke(self._handle_after_invoke)
-
 
     async def _handle_before_invoke(self, ctx):
         self.current_context = ctx
@@ -50,100 +49,10 @@ class Stefan(commands.Bot):
             else:
                 await self.current_context.author.voice.channel.connect()
 
+    async def on_ready(self):
+        print("Stefan anmäler sig för tjänstgöring.")
+        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="ert skitsnack"))
 
 stefan = Stefan(command_prefix=config.get("prefix"))
 stefan.add_cog(Music(stefan))
-
-@stefan.event
-async def on_ready():
-    print("Stefan anmäler sig för tjänstgöring.")
-    await stefan.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="ert skitsnack"))
-
-@stefan.command()
-async def ping(ctx):
-    """ TODO: Write docstring """
-    await ctx.message.channel.send("Pong!")
-
-@stefan.command(name = "prefix")
-async def prefix(ctx, new_prefix):
-    """ TODO: Write docstring """
-
-    stefan.command_prefix = new_prefix
-    await config.set("prefix", new_prefix)
-
-    await ctx.message.channel.send("Tack för mitt nya prefix! 🥰")
-
-@stefan.command(name = "kom", aliases = ["komsi", "älskling", "hit"])
-async def kom(ctx, arg1="", arg2="", arg3=""):
-    """ TODO: Write docstring """
-    cm = " ".join(filter(None, [ctx.invoked_with, arg1, arg2, arg3]))
-    # await ctx.send("cm: " + cm + "\narg1: " + arg1 + "\narg2: " + arg2 + "\narg3: " + arg3)
-    if cm in ["kom", "kom hit", "komsi komsi", "älskling jag är hemma", "hit"]:
-        # If user is in a channel
-        if ctx.author.voice:
-            # If bot is in a channel
-            if ctx.guild.voice_client:
-                # If they are in the same channel
-                if ctx.author.voice.channel == ctx.guild.voice_client.channel:
-                    await ctx.send("Jag är redan här för dig! 🥰")
-                # If they are in different channels
-                else:
-                    # If bot has company
-                    if len(ctx.guild.voice_client.channel.members) > 1:
-                        await ctx.send("Jag är upptagen! 😤")
-                    # If bot is alone
-                    else:
-                        await ctx.send("Äntligen lite sällskap igen! 😊")
-                        await ctx.guild.voice_client.disconnect()
-                        await ctx.author.voice.channel.connect()
-            # If user is in a channel, but bot is not
-            else:
-                await ctx.send("Jag kommer! 😁")
-                await ctx.author.voice.channel.connect()
-        # If user is not in a channel
-        else:
-            await ctx.send("Jag vet inte vart jag ska. 😢")
-    else:
-        pass
-
-@stefan.command(name = "stick", aliases = ["schas", "försvinn", "dra", "gå"])
-async def stick(ctx):
-    """ TODO: Write docstring """
-    # If bot is in a channel
-    if ctx.guild.voice_client:
-        # If user is in a channel
-        if ctx.author.voice:
-            # If they are in the same channel
-            if ctx.author.voice.channel == ctx.guild.voice_client.channel:
-                await ctx.send('Okej då... 😥')
-                await ctx.guild.voice_client.disconnect()
-            # If they are in different channels
-            else:
-                # If bot has company
-                if len(ctx.guild.voice_client.channel.members) > 1:
-                    await ctx.send("Förstör inte det roliga! 😠")
-                # If bot is alone
-                else:
-                    await ctx.send('Okej då... 😥')
-                    await ctx.guild.voice_client.disconnect()
-        # If bot is in a channel, but user is not
-        else:
-            # If bot has company
-            if len(ctx.guild.voice_client.channel.members) > 1:
-                await ctx.send("Förstör inte det roliga! 😠")
-            # If bot is alone
-            else:
-                await ctx.send('Okej då... 😥')
-                await ctx.guild.voice_client.disconnect()
-    # If bot is not in a channel
-    else:
-        await ctx.send("Jag har redan stuckit ju! 💔")
-
-@stefan.command(name = "hjälp", aliases = ["hilfe", "aidez-moi", "h"])
-async def hjälp(ctx):
-    """ TODO: Write docstring """
-    embed=Embed(title="Mina kommandon 😎 :", color=Color.orange(), description = f"Genom att skriva \"{stefan.command_prefix}\" följt av ett av nedanstående kommandon kan du få mig att göra roliga saker! Lek med mig! 🥰")
-    embed.add_field(name="**stick / gå / schas / försvinn / dra**", value="Säg åt mig att lämna röstkanalen. 😥", inline=False)
-    embed.add_field(name="**kom / hit / komsi komsi / älskling jag är hemma**", value="Be mig att göra dig sällskap! 😇", inline=False)
-    embed.add_field(name="**prefix**", value="Ge mig ett nytt prefix som jag kan lyssna på! ☺️")
-    await ctx.send(embed=embed)
+stefan.add_cog(Misc(stefan))
